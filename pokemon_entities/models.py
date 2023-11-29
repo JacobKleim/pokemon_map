dd08fd2ac71f7300c46c61b9b92020d25d2d6a97
JacobKleim <yakovkleimenov@yandex.ru>
2023-11-29 19:21:10 +0400

    Show pokemons on mainpage map

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,5 +1,7 @@
 from django.db import models 
 
+from datetime import datetime
+
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(blank=True, null=True)
@@ -12,8 +14,8 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     lat = models.FloatField()
     lon = models.FloatField()
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
+    appeared_at = models.DateTimeField(default=datetime(2023, 1, 2))
+    disappeared_at = models.DateTimeField(default=datetime(2023, 1, 2))
     level = models.IntegerField(blank=True, null=True)
     health = models.IntegerField(blank=True, null=True)
     strenght = models.IntegerField(blank=True, null=True)
